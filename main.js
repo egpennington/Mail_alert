17176653b9c2bdcf06e5c2e7687b1f600562318d
egpennington <53251137+egpennington@users.noreply.github.com>
2021-12-30 11:52:18 -0500

    Add files via upload
    uploaded first mockup version

---
+++ main.js
@@ -0,0 +1,11 @@
+/* javascript for mail alert app https://www.w3schools.com/jsref/event_onclick.asp */
+
+
+function addName() {
+  var name = prompt("What is your name?");
+  alert("Hi " + name + " You have been added to Mail Alert!");
+}
+
+function currentTime() {
+  alert("The current time is...");
+}
